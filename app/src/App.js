9db6e080c3a9614b21bc6ac3a1f05d784ec87a0a
PedroHenriqueBragaDaSilva <pedrohenriquebraga735@gmail.com>
2021-09-08 20:30:52 -0300

    add colors

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import './App.css';
+import styled, {ThemeProvider} from 'styled-components';
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  color: ${props => props.theme.primary};
+`;
+
+const theme = {
+  primary: '#5F1478',
+  secondary: '#AE276F',
+  tertiary: '#E76316',
+  white: '#FFFFFF',
+  gray1: '#F6F4F6',
+  gray2: '#E5E5E5',
+  gray3: '#E4E4E4',
+  gray4: '#0000000F',
+  black: '#263238',
+  graySeparator: '#F0EFF0',
+  pinkShadow: '#92207242',
+  shadowAppbar: '#00000029',
+  darkGray: '#757575',
+};
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppContainer>
+        <h1>Hello World</h1>
+      </AppContainer>
+    </ThemeProvider>
   );
 }
 
