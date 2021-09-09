b88f130cba210a2109ea57ef46029c127ac4fb2f
PedroHenriqueBragaDaSilva <pedrohenriquebraga735@gmail.com>
2021-09-08 22:34:10 -0300

    Banner and Background

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import AppBar from './components/AppBar';
+import Form from './components/Form';
+import Banner from './img/fundo-busca.png';
 
 const AppContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `;
+
+const BannerBackground  = styled.div`
+    background-image: url(${Banner});
+    width: 100%;
+    height:261px;
+    z-index: -1;
+    top:0;
+    left:0;
+    position: absolute;
+`;
+
+
 function App() {
   return (
       <AppContainer>
       <AppBar />
+      <BannerBackground />
+      <Form></Form>
       </AppContainer>
   );
 }
---
+++ app/src/components/Form/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 147px;;
+`;
+
+export default function Form(props) {
+  return (
+    <Container>
+        <p>opa</p>
+    </Container>
+  )
+}
