0e86000f1458df77da27214717e1ed379ac8fc63
Eduarda Almeida <eduardapalm27@gmail.com>
2021-10-16 07:04:37 -0300

    Update: add logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,10 @@
-import react = require('react');
+import react from 'react'
+import Logo from '../../assets/logo-teste.svg'
 
 export default function Header(){
   return(
-
+    <>
+      <img src={Logo}/>
+    </>
   )
 }
