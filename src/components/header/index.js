9a1d88cfd40d4bf39194d7b814c9fb27776f5df3
Eduarda Almeida <eduardapalm27@gmail.com>
2021-10-16 13:25:57 -0300

    Fix: Camel Case

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,11 @@
-import react from 'react'
+import React from 'react'
 import Logo from '../../assets/logo-teste.svg'
 import * as S from '../../style/header.js'
 
 export default function Header(){
   return(
-    <S.header>
-      <S.image src={Logo}/>
-    </S.header>
+    <S.Header>
+      <S.Image src={Logo}/>
+    </S.Header>
   )
 }
--- src/style/header.js
+++ src/style/header.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
-export const header = styled.header`
+export const Header = styled.header`
   background: transparent linear-gradient(272deg, #AE276F 0%, #5F1478 100%) 0% 0% no-repeat padding-box;
   height: 4rem;
   width: 100vw;
 `
-export const image = styled.img`
+export const Image = styled.img`
   width: 12rem;
   height: 2.3125rem;
   margin: 0.8369rem 1.4981rem;
