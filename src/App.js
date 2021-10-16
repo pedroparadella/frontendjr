c1a058a45e9628bcec78e5762a1117b021abeadb
Eduarda Almeida <eduardapalm27@gmail.com>
2021-10-16 13:22:54 -0300

    Update: styling App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import SearchBar from './components/searchBar'
 import Header from './components/header/index.js'
+import * as S from './style/app.js'
 
 function App() {
   return (
-    <>
+    <S.Wrapper>
       <Header/>
       <SearchBar/>
-    </>
+    </S.Wrapper>
   );
 }
 
---
+++ src/style/app.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  position: absolute;
+  width: 100vw;
+  overflow-x: hidden;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  left: 0;
+  top: 0;
+  background-color: #F6F4F6;
+`
