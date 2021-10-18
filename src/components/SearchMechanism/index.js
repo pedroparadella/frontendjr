d4b75ae76ab39960e2d9df6fbe07c9215f912f9a
Eduarda Almeida <eduardapalm27@gmail.com>
2021-10-18 14:33:54 -0300

    Add: adding modal to the search page

--- src/components/SearchMechanism/index.js
+++ src/components/SearchMechanism/index.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { cloneElement, useState } from "react";
 import { api } from "../../services/goat-api";
+import Lottie from "react-lottie";
+import animationData from "../../animations/x.json";
 import * as S from "../../style/search";
+import * as M from "../../style/modal";
 import lupa from "../../assets/lupa.svg";
 import trash from "../../assets/Icon-trash.svg";
-import edit from "../../assets/Icon-edit.svg"
+import edit from "../../assets/Icon-edit.svg";
 
 export default function SearchMechanism() {
+  const [open, setOpen] = useState(false);
   const [searched, setSearched] = useState();
   const [characterName, setCharacterName] = useState("");
-  const getCharacterName = (event) => setCharacterName(event.target.value);
+  const [animationState, setAnimationState] = useState({
+    isStopped: false,
+    isPaused: false,
+  });
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  function getCharacterName(event) {
+    setCharacterName(event.target.value);
+  }
 
   async function searchCharacter() {
     const response = await api.get(`character/?name=${characterName}`);
@@ -16,8 +36,33 @@ export default function SearchMechanism() {
     console.log(response.data);
   }
 
+  function handleClick() {
+    setOpen(true);
+  }
+
+  function closeModal() {
+    setOpen(false);
+  }
+
   return (
     <main>
+      {open ? (
+        <M.Modal onClick={closeModal}>
+          <M.Card>
+            <Lottie
+              options={defaultOptions}
+              height={100}
+              width={100}
+              isStopped={setAnimationState.isStopped}
+              isPaused={setAnimationState.isPaused}
+            />
+            <M.H3>Ops!</M.H3>
+            <M.P>Essa funcionalidade ainda não foi implementada.</M.P>
+
+            <M.Button onClick={closeModal}>Fechar</M.Button>
+          </M.Card>
+        </M.Modal>
+      ) : null}
       <S.Background>
         <S.Input
           type="text"
@@ -32,33 +77,35 @@ export default function SearchMechanism() {
       <S.Section>
         <S.PreCardsLine>
           <S.H1>Resultados de busca</S.H1>
-          <S.NewCardButton>Novo Card</S.NewCardButton>
+          <S.NewCardButton onClick={handleClick}>Novo Card</S.NewCardButton>
         </S.PreCardsLine>
-
         <S.Cards>
           {searched &&
-            searched.results.map((character, i) => (
-              i <= 10 ?
-              <S.Card>
-                <S.Content>
+            searched.results.map((character, i) =>
+              i < 12 ? (
+                <S.Card>
+                  <S.Content>
                     <S.Image src={character.image} alt="" />
-                    <S.Hr/>
+                    <S.Hr />
                     <S.H2>{character.name}</S.H2>
                     <S.P>{character.location.name}</S.P>
-                </S.Content>
-                <S.Options>
-                  <S.Icons src={trash} alt="" />
-                  <S.POptions>Excluir</S.POptions>
-                  <S.OHr />
-                  <S.Icons src={edit} alt="" />
-                  <S.POptions>Editar</S.POptions>
-                </S.Options>
-              </S.Card>
-              :
-
+                  </S.Content>
+                  <S.Options>
+                    <S.Option onClick={handleClick}>
+                      <S.Trash src={trash} alt="" />
+                      <S.POptions>Excluir</S.POptions>
+                    </S.Option>
+                    <S.OHr />
+                    <S.Option onClick={handleClick}>
+                      <img src={edit} alt="" />
+                      <S.POptions>Editar</S.POptions>
+                    </S.Option>
+                  </S.Options>
+                </S.Card>
+              ) : (
                 <></>
-            ))
-          }
+              )
+            )}
         </S.Cards>
       </S.Section>
     </main>
