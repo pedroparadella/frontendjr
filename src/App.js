c9ae3c45833767d2742b5b5e3443ee03944bf80f
Eduarda Almeida <eduardapalm27@gmail.com>
2021-10-18 08:28:34 -0300

    Update: rendering modal on App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SearchMechanism from './components/SearchMechanism'
 import Header from './components/header/index.js'
+import Modal from './components/modal'
 import * as S from './style/app.js'
 
 function App() {
   return (
     <S.Wrapper>
+      <Modal/>
       <Header/>
       <SearchMechanism/>
     </S.Wrapper>
