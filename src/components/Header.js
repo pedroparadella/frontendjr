ccda47f3ff9ef84610f7e19c46f39d46b1c087cb
rodrigoorocha <rodrigotherock@hotmail.com>
2021-12-19 14:03:45 -0300

    primeiro commit

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+
+
+
+
+function Header() {
+    return (
+
+        <div className="App">
+
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item">
+                        <img id="logoImpar" class="is-rounded" src="https://media-exp1.licdn.com/dms/image/C4D0BAQFvcrusz1Mhiw/company-logo_200_200/0/1627916118438?e=2159024400&v=beta&t=vZA-Tb0krYlKpm1VuKJXraPTa3SnBGAEgMKyYuG3sqY" />
+                         <b>Impar</b>
+                    </a>
+                </div>
+            </nav>
+
+            <br/>
+
+
+        </div>
+    );
+}
+
+export default Header;
