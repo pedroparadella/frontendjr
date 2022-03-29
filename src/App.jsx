17df7d1aeab081382a763c6ad78aa06553239664
Carlo Ryoshin <cryofreelance@gmail.com>
2022-03-29 10:35:59 -0300

    App strcuture defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 
+//App will contain a Navbar, NewButton, Cards, New Card Module and Search;
+
 function App() {
   return (
-    <p>
-      Cleanedup React App
-    </p>
+    <NewCard />
+    <Navbar />
+    <Search />
+    <NewButton />
+    <Cards />
   );
 }
 
