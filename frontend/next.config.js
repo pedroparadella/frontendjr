391defff080b8730450c6b241a554219d7484cbd
Erick-Marinho <erickgaves@gmail.com>
2022-04-05 17:41:59 -0300

    :wrench: Config imagens domains

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: [
+			'media0.giphy.com',
+			'media1.giphy.com',
+			'media2.giphy.com',
+			'media3.giphy.com',
+			'media4.giphy.com',
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
