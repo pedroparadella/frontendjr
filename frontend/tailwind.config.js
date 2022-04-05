3182c09c93895379433b55b010f33aa029060e2f
Erick-Marinho <erickgaves@gmail.com>
2022-04-05 17:42:25 -0300

    :wrench: Config bg-imagem

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,9 +2,14 @@ module.exports = {
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
+		'./node_modules/flowbite/**/*.js',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'fundo-busca': "url('/fundo-busca.png')",
+			},
+		},
 	},
 	plugins: [],
 };
