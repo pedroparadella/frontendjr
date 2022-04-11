85b85c64a387dd4442e14f67099b7ccdaac35127
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2022-04-11 17:43:06 -0300

    Iniciando componetização do Carousel ( flat-carousel )

---
+++ impar/src/components/Card/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div `
+  height: 7vh;
+  padding-left: 2vw;
+  width: 100vw;
+`;
--- impar/src/components/Carousel/index.jsx
+++ impar/src/components/Carousel/index.jsx
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import whiteLogo from "../../assets/white-logo.svg";
+import Carousel from 'flat-carousel';
+import './styles.css'
 
-class DemoCarousel extends Component {
-    render() {
-        return (
-            <Carousel>
-                <div>
-                    <img src={whiteLogo} />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src="assets/2.jpeg" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="assets/3.jpeg" />
-                    <p className="legend">Legend 3</p>
-                </div>
-            </Carousel>
-        );
-    }
-};
+const images = [
+    { src: 'https://reactjs.org/logo-og.png' },
+    { src: 'https://reactjs.org/logo-og.png' }
 
-export default DemoCarousel
+];
+
+const CarouselContainer = () => (
+    <Carousel>
+        {images.map((image, index) => (
+            <div
+                key={index}
+                className="demo-item"
+                style={{ backgroundImage: 'url(' + image.src + ')' }}
+            />
+        ))}
+    </Carousel>
+);
+
+export default CarouselContainer
