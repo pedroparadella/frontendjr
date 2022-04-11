6f9011b4cb9c1e6ab8b9ee2172a524d44aca6590
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2022-04-11 18:20:32 -0300

    NavBar -> ajuste

--- impar/src/components/NavBar/styles.js
+++ impar/src/components/NavBar/styles.js
@@ -6,11 +6,11 @@ export const NavBarContainer = styled.div `
     rgb(99, 21, 119),
     rgb(169, 38, 112)
   );
-  height: 7vh;
+  height: 10vh;
   padding-left: 2vw;
   width: 100vw;
 `;
 
 export const WhiteLogo = styled.img `
-width: 11vw
+width: 15vw
 `
