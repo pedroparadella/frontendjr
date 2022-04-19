2729d939aa5b0d2696600d5e77dbf4a37106de0b
Frederico Canoeira <fcanoeira@gmail.com>
2022-04-19 15:17:32 +0100

    Implementing all funcionality

---
+++ search_page/src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Developed by <a href="https://www.linkedin.com/in/fredericocanoeira/">Frederico Canoeira</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ search_page/src/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import FavoriteContext from "./FavoritesContext";
+
+const { useContext } = React;
+
+const Navbar = () => {
+  const { favoritePokemons } = useContext(FavoriteContext);
+
+  let imgUrl =
+    "hhttps://impar.com.br/img/Impar_logo_branco.svg";
+
+  return (
+    <nav>
+      <div />
+      <div>
+        <img src={imgUrl} alt="impar-logo" className="navbar-image" />
+      </div>
+      <div>&#10084;&#65039; {favoritePokemons.length}</div>
+    </nav>
+  );
+};
+
+export default Navbar;
