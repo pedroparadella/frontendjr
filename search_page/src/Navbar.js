ee67f537e3841ebe1602db128528de19bdc362c3
Frederico Canoeira <fcanoeira@gmail.com>
2022-04-21 01:02:33 +0100

    Finalizing deployments

--- search_page/src/Navbar.js
+++ search_page/src/Navbar.js
@@ -1,10 +1,6 @@
 import React from "react";
-/* import FavoriteContext from "./FavoritesContext"; */
-
-/* const { useContext } = React; */
 
 const Navbar = () => {
-  /* const { favoritePokemons } = useContext(FavoriteContext); */
 
   let imgUrl =
     "https://impar.com.br/img/Impar_logo_branco.svg";
@@ -15,7 +11,6 @@ const Navbar = () => {
       <div>
         <img src={imgUrl} alt="Teste-logo" className="navbar-image" />
       </div>
-      {/* <div>&#10084;&#65039; {favoritePokemons.length}</div> */}
     </nav>
   );
 };
